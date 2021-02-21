eb02f3b675363bdce534286dd5faee25ff8182f0
Dratik <ipratikdeshmukh@gmail.com>
2021-02-21 15:32:18 +0530

    AppBar added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import ButtonAppBar  from './appbar/Appbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+    <ButtonAppBar />
+      <div className="App">
         <p>
-          Edit <code>src/Aphhhhhhp.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Hello User!
+      </p>
+      </div>
+    </>
   );
 }
 
