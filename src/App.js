d04410403f87b5707736c17b37efd723a5c90d0f
Dratik <ipratikdeshmukh@gmail.com>
2021-03-04 14:45:27 +0530

    label changed in app.js file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
 import AppMenu from './appbar/AppMenu';
 import Login from './components/login&Signup/login/Login';
-import Register from './components/login&Signup/register/Register';
-import FooBarForm from './components/SampleForm';
 
 function App() {
   return (
     <>
       <AppMenu />
       <div className="App">
-        <p>Hello User!</p>
+        <h4>Hello Pratik Here!</h4>
         <Login />
-        {/* <Register /> */}
       </div>
     </>
   );
